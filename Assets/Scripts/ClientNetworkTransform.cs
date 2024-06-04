1995a0a205dba450fff96febb1829d5ae52c7e0e
Rafael José <ra.david@hotmail.com>
2024-06-04 13:28:06 +0100

    Add ClientNetworkTransform for player position and rotation

---
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+
+public class ClientNetworkTransform : NetworkTransform
+{
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        CanCommitToTransform = IsOwner;
+    }
+
+    override protected void Update()
+    {
+        CanCommitToTransform = IsOwner;
+        base.Update();
+
+        if (NetworkManager != null)
+        {
+            if (NetworkManager.IsConnectedClient || NetworkManager.IsListening)
+            {
+                if (CanCommitToTransform)
+                {
+                    TryCommitTransformToServer(transform, NetworkManager.LocalTime.Time);
+                }
+            }
+
+        }
+    }
+
+    protected override bool OnIsServerAuthoritative()
+    {
+        return false;
+    }
+}
