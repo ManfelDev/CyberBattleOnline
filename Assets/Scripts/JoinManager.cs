d06ef37dabc359744696049920d2e5bcb4bf61bc
Rafael José <ra.david@hotmail.com>
2024-06-07 17:10:44 +0100

    Update player name character limit

--- Assets/Scripts/JoinManager.cs
+++ Assets/Scripts/JoinManager.cs
@@ -6,16 +6,27 @@ public class JoinManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputFieldJoinCode;
     [SerializeField] private TMP_InputField inputFieldPlayerName;
+    [SerializeField] private int            maxPlayerNameLength = 12;
     [SerializeField] private GameObject     joinUI;
     [SerializeField] private NetworkSetup   networkSetup;
 
     public static string             joinCode;
     public static FixedString32Bytes playerName;
 
+    private void Start()
+    {
+        inputFieldPlayerName.characterLimit = maxPlayerNameLength;
+    }
+
     public void OnJoinButtonClicked()
     {
         joinCode    = inputFieldJoinCode.text;
         playerName  = inputFieldPlayerName.text;
+
+        if (playerName.Length < 1)
+        {
+            playerName = "Player";
+        }
         
         StartCoroutine(networkSetup.StartAsClientCR());
 
