976cec8c719bdd43e646bda14a0d0d2642865863
Rafael José <ra.david@hotmail.com>
2024-06-07 16:56:59 +0100

    Update healing space for restoring health

--- Assets/Scripts/HealingSpace.cs
+++ Assets/Scripts/HealingSpace.cs
@@ -6,11 +6,40 @@ using UnityEngine;
 
 public class HealingSpace : NetworkBehaviour
 {
-    [SerializeField] private GameObject      ActiveModel;
-    [SerializeField] private GameObject      InactiveModel;
-    [SerializeField] private TextMeshProUGUI timerText;
-    [SerializeField] private float           healAmount = 50;
-    [SerializeField] private float           healCooldown = 15;
+    [SerializeField] private GameObject         ActiveModel;
+    [SerializeField] private GameObject         InactiveModel;
+    [SerializeField] private TextMeshProUGUI    timerText;
+    [SerializeField] private int                healAmount = 50;
+    [SerializeField] private float              healCooldown = 15;
+
+    private NetworkVariable<float> remainingHealCooldown = new NetworkVariable<float>();
+    private NetworkVariable<bool> isActive = new NetworkVariable<bool>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            isActive.OnValueChanged += OnActiveChanged;
+            remainingHealCooldown.OnValueChanged += OnCooldownChanged;
+            OnActiveChanged(true, isActive.Value);
+            OnCooldownChanged(0, remainingHealCooldown.Value);
+        }
+
+        if (IsServer)
+        {
+            isActive.Value = true;
+            remainingHealCooldown.Value = 0;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            isActive.OnValueChanged -= OnActiveChanged;
+            remainingHealCooldown.OnValueChanged -= OnCooldownChanged;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,7 +47,43 @@ public class HealingSpace : NetworkBehaviour
 
         if (other.TryGetComponent(out Player player))
         {
-            Debug.Log($"Player {player.playerName.Value} entered the healing space");
+            if (isActive.Value)
+            {
+                if (player.Health.CurrentHealth.Value == player.Health.MaxHealth) return;
+                
+                player.Health.Heal(healAmount);
+
+                isActive.Value = false;
+
+                remainingHealCooldown.Value = healCooldown;
+            }
         }
+    }
+
+    private void Update()
+    {
+        if (!IsServer) return;
+
+        if (remainingHealCooldown.Value > 0)
+        {
+            remainingHealCooldown.Value -= Time.deltaTime;
+
+            if (remainingHealCooldown.Value <= 0)
+            {
+                isActive.Value = true;
+                remainingHealCooldown.Value = 0;
+            }
+        }
+    }
+
+    private void OnCooldownChanged(float previousValue, float newValue)
+    {
+        timerText.text = newValue > 0 ? $"{newValue:F0}" : string.Empty;
+    }
+
+    private void OnActiveChanged (bool previousValue, bool newValue)
+    {
+        ActiveModel.SetActive(newValue);
+        InactiveModel.SetActive(!newValue);
     }
 }
